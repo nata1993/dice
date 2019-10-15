2ce51b255e8b05da838e6930e8bef99d6971c4f3
kebo <bogdan.parubok@hotmail.com>
2019-10-15 20:25:24 +0300

    made dice nr 2 - the visual one

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -58,45 +58,6 @@ namespace Kivipaberkäärid
                 Console.WriteLine("tie");
             }
             Console.WriteLine($"your score {userScore}, cpu score {cpuScore}");
-
-            Console.ReadLine();
-            //KIVIPABERKÄÄRID
-            /*Console.WriteLine("Hello World! Lets play kivi paber käärid!");
-            value = Console.ReadLine();
-            Console.WriteLine($"Your weapon is {value}.");
-
-            Random randomGenerator = new Random();
-            int randomNumber = randomGenerator.Next(1, 4);
-
-            if (randomNumber == 1) 
-                {
-                Console.WriteLine("My weapon is Kivi");
-                }
-
-            else if (randomNumber == 2)
-            {
-                Console.WriteLine("My weapon is Käärid");
-            }
-
-            else
-            {
-                Console.WriteLine("My weapon is Paber");
-            }
-
-            if (randomNumber > value)
-            {
-                Console.WriteLine("You loose");
-            }
-
-            else if (randomNumber < value)
-            {
-                Console.WriteLine("You win");
-            }
-
-            else
-            {
-                Console.WriteLine("Tie");
-            }*/
             Console.ReadLine();
         }
     }
