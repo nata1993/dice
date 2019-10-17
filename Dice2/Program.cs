4db4d14ad75949dbf73266c28ace7cc467b1e065
kebo <bogdan.parubok@hotmail.com>
2019-10-17 20:48:37 +0300

    added cash and odd/even/exit prior rollind dice

--- Dice2/Program.cs
+++ Dice2/Program.cs
@@ -13,6 +13,7 @@ namespace Dice2
 
             int rndGen1; //first dice
             int rndGen2; //second dice
+            int userCash; //user inserted money
 
             Console.WriteLine("Hello! This is \"DICE\". This program rolls dice for you just like you would roll them in Casino.");
             Console.WriteLine("Lets roll the dice!");
@@ -43,142 +44,182 @@ namespace Dice2
 
                 else if (userChoice1.ToUpper() == "Y") //roll dice if Y
                 {
-                    Random rndNr1 = new Random(); //Dice nr 1
-                    rndGen1 = rndNr1.Next(1, 7);
-
-                    switch (rndGen1)
-                    {
-                        case 1:
-                            Console.WriteLine(" __________________");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|        O         |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|__________________|");
-                            break;
-                        case 2:
-                            Console.WriteLine(" __________________");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("| O                |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|               O  |");
-                            Console.WriteLine("|__________________|");
-                            break;
-                        case 3:
-                            Console.WriteLine(" __________________");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|  O               |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|         O        |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|               O  |");
-                            Console.WriteLine("|__________________|");
-                            break;
-                        case 4:
-                            Console.WriteLine(" __________________");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|  O            O  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|  O            O  |");
-                            Console.WriteLine("|__________________|");
-                            break;
-                        case 5:
-                            Console.WriteLine(" __________________");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|  O            O  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|         O        |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|  O            O  |");
-                            Console.WriteLine("|__________________|");
-                            break;
-                        case 6:
-                            Console.WriteLine(" __________________");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|  O            O  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|  O            O  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|  O            O  |");
-                            Console.WriteLine("|__________________|");
-                            break;
-                    }
-
-                    Random rndNr2 = new Random(); //Dice nr 2
-                    rndGen2 = rndNr2.Next(1, 7);
-
-                    switch (rndGen2)
-                    {
-                        case 1:
-                            Console.WriteLine(" __________________");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|        O         |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|__________________|");
-                            break;
-                        case 2:
-                            Console.WriteLine(" __________________");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("| O                |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|               O  |");
-                            Console.WriteLine("|__________________|");
-                            break;
-                        case 3:
-                            Console.WriteLine(" __________________");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|  O               |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|         O        |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|               O  |");
-                            Console.WriteLine("|__________________|");
-                            break;
-                        case 4:
-                            Console.WriteLine(" __________________");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|  O            O  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|  O            O  |");
-                            Console.WriteLine("|__________________|");
-                            break;
-                        case 5:
-                            Console.WriteLine(" __________________");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|  O            O  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|         O        |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|  O            O  |");
-                            Console.WriteLine("|__________________|");
-                            break;
-                        case 6:
-                            Console.WriteLine(" __________________");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|  O            O  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|  O            O  |");
-                            Console.WriteLine("|                  |");
-                            Console.WriteLine("|  O            O  |");
-                            Console.WriteLine("|__________________|");
-                            break;
-                    }
-
+                    CashAgain:
                     Console.WriteLine("\n");
-                    Console.WriteLine("Press Y to roll dice or N if you want to exit program TÜRA.");
+                    Console.WriteLine("Please, insert Cash.");
+                    userCash = int.Parse(Console.ReadLine());
+
+                    if (userCash == 0)
+                    {
+                        goto CashAgain;
+                    }                    
+
+                    while (userCash > 0 && diceRun1 == false)
+                    {
+                        Console.WriteLine("\n");
+                        Console.WriteLine("ODD or EVEN. Press N to exit.");
+                        userChoice2 = Console.ReadLine();
+
+                        switch (userChoice2.ToUpper())
+                        {
+                            case "ODD":
+                                break;
+                            case "EVEN":
+                                break;
+                            case "N":
+                                break;
+                            default:
+                                Console.WriteLine("Insert ODD or EVEN. Press N to EXIT.");
+                                break;
+                        }
+
+                        if (userChoice2.ToUpper() == "N")
+                        {
+                            diceRun1 = true;
+                            Console.WriteLine("\n");
+                            Console.WriteLine("Good bye!");
+                        }
+
+                        else
+                        {
+                            Random rndNr1 = new Random(); //Dice nr 1
+                            rndGen1 = rndNr1.Next(1, 7);
+
+                            switch (rndGen1)
+                            {
+                                case 1:
+                                    Console.WriteLine(" __________________");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|        O         |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|__________________|");
+                                    break;
+                                case 2:
+                                    Console.WriteLine(" __________________");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("| O                |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|               O  |");
+                                    Console.WriteLine("|__________________|");
+                                    break;
+                                case 3:
+                                    Console.WriteLine(" __________________");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|  O               |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|         O        |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|               O  |");
+                                    Console.WriteLine("|__________________|");
+                                    break;
+                                case 4:
+                                    Console.WriteLine(" __________________");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|  O            O  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|  O            O  |");
+                                    Console.WriteLine("|__________________|");
+                                    break;
+                                case 5:
+                                    Console.WriteLine(" __________________");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|  O            O  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|         O        |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|  O            O  |");
+                                    Console.WriteLine("|__________________|");
+                                    break;
+                                case 6:
+                                    Console.WriteLine(" __________________");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|  O            O  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|  O            O  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|  O            O  |");
+                                    Console.WriteLine("|__________________|");
+                                    break;
+                            }
+
+                            Random rndNr2 = new Random(); //Dice nr 2
+                            rndGen2 = rndNr2.Next(1, 7);
+
+                            switch (rndGen2)
+                            {
+                                case 1:
+                                    Console.WriteLine(" __________________");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|        O         |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|__________________|");
+                                    break;
+                                case 2:
+                                    Console.WriteLine(" __________________");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("| O                |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|               O  |");
+                                    Console.WriteLine("|__________________|");
+                                    break;
+                                case 3:
+                                    Console.WriteLine(" __________________");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|  O               |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|         O        |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|               O  |");
+                                    Console.WriteLine("|__________________|");
+                                    break;
+                                case 4:
+                                    Console.WriteLine(" __________________");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|  O            O  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|  O            O  |");
+                                    Console.WriteLine("|__________________|");
+                                    break;
+                                case 5:
+                                    Console.WriteLine(" __________________");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|  O            O  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|         O        |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|  O            O  |");
+                                    Console.WriteLine("|__________________|");
+                                    break;
+                                case 6:
+                                    Console.WriteLine(" __________________");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|  O            O  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|  O            O  |");
+                                    Console.WriteLine("|                  |");
+                                    Console.WriteLine("|  O            O  |");
+                                    Console.WriteLine("|__________________|");
+                                    break;
+                            }
+
+                            Console.WriteLine("\n");
+                            Console.WriteLine("Press Y to roll dice or N if you want to exit program TÜRA.");
+                        }
+                    }
                 }                
             }
         }
